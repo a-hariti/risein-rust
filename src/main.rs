3b41037a1d7612c7c14e8d8cc02175a0757c54db
Abdellah Hariti <haritiabdellah@gmail.com>
2025-07-07 00:46:41 +0100

    task 1

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    crate::task1::main();
+}
+
+mod task1 {
+    pub fn concatenate_strings(s1: &str, s2: &str) -> String {
+        let mut result = String::new();
+        result.push_str(s1);
+        result.push_str(s2);
+        result
+    }
+    pub fn main() {
+        let string1 = String::from("Hello");
+        let string2 = String::from(" World");
+        let result = crate::task1::concatenate_strings(&string1, &string2);
+        println!("Concatenated string: {}", result);
+    }
+}
