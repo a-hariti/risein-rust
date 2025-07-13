ca7c3514dac65195f6d42491c94eacc80d1b81e3
Abdellah Hariti <haritiabdellah@gmail.com>
2025-07-13 13:58:34 +0100

    implement task 2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     crate::task1::main();
+    crate::task2::main();
 }
 
 mod task1 {
@@ -14,5 +15,68 @@ mod task1 {
         let string2 = String::from(" World");
         let result = concatenate_strings(&string1, &string2);
         println!("Concatenated string: {}", result);
+    }
+}
+
+mod task2 {
+    use std::{io, str::FromStr};
+
+    #[derive(Debug)]
+    pub enum Operation {
+        Add,
+        Subtract,
+        Multiply,
+        Divide,
+    }
+
+    impl FromStr for Operation {
+        type Err = String;
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            match s {
+                "add" => Ok(Operation::Add),
+                "subtract" => Ok(Operation::Subtract),
+                "multiply" => Ok(Operation::Multiply),
+                "divide" => Ok(Operation::Divide),
+                _ => Err(format!("Invalid operation: {}", s)),
+            }
+        }
+    }
+    pub fn calculate(op: Option<Operation>, a: f64, b: f64) -> Option<f64> {
+        match op {
+            Some(Operation::Add) => Some(a + b),
+            Some(Operation::Subtract) => Some(a - b),
+            Some(Operation::Multiply) => Some(a * b),
+            Some(Operation::Divide) => Some(a / b),
+            None => None,
+        }
+    }
+    pub fn main() {
+        // prompt user for inputs (op, a and b) from stdin
+        let mut input = String::new();
+
+        input.clear();
+        println!("Enter first number: ");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("failed to read stdin");
+        let a: f64 = input.trim().parse().expect("failed to parse a");
+        // empty the input buffer
+        input.clear();
+
+        println!("Enter operation (add, subtract, multiply, divide): ");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("failed to read stdin");
+        let op = input.trim().parse::<Operation>().ok();
+        // empty the input buffer
+        input.clear();
+
+        println!("Enter second number: ");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("failed to read stdin");
+        let b: f64 = input.trim().parse().expect("failed to parse b");
+        let result = calculate(op, a, b);
+        println!("Result: {:?}", result);
     }
 }
